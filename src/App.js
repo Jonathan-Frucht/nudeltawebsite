24fe92731ecd41f958fe877fc1c493e7d9f2208f
Jonathan Frucht <jonfrucht@gmail.com>
2020-12-16 18:45:48 -0500

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import "./App.css";
-import InitPage from "./pages/InitPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import ExecPage from "./pages/ExecPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <InitPage />
-    </div>
+    <Router>
+      <Switch>
+        {/* <Route exact path="/" component={}/> */}
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/exec_board" component={ExecPage} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+    //   <div className="App">
+    //     <InitPage />
+    //   </div>
   );
 }
 
