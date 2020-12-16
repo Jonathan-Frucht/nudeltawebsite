eee90a2e5834bc164f834101282e0f4934fdd0cb
Jonathan Frucht <jonfrucht@gmail.com>
2020-12-16 12:26:18 -0500

    Created Sidebar

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,30 +1,66 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Bofa from "../pictures/COFA_Color.png";
+import Image from "react-bootstrap/Image";
 function MainNavBar() {
   return (
-    <div>
-      <Navbar className="navbar-custom">
-        <Navbar.Brand>
-          <img src="" className="d-inline-block align-top" alt="..." />
-        </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <ul>
-            <Navbar.Text style={{ margin: "10px" }}>
-              <a href="">Alpha Epsilon Pi</a>
-            </Navbar.Text>
-            <Navbar.Text style={{ margin: "10px" }}>
-              <a href="">Meet the Central Comm.</a>
-            </Navbar.Text>
-            <Navbar.Text style={{ margin: "10px" }}>
-              <a href="">Our manifesto</a>
-            </Navbar.Text>
-            <Navbar.Text style={{ margin: "10px" }}>
-              <a href="">Join the CCP!</a>
-            </Navbar.Text>
-          </ul>
-        </Navbar.Collapse>
-      </Navbar>
+    // <div className="wrapper">
+    //   <Navbar id="sidebar">
+    //     <Navbar.Brand>
+    //       <img src={Bofa} alt="..." width="220px" height="273px" />
+    //     </Navbar.Brand>
+
+    //     <Navbar.Collapse id="content">
+    //       <ul>
+    //         <li>
+    //           {" "}
+    //           <Navbar.Text style={{ margin: "10px" }}>
+    //             <a href="">Alpha Epsilon Pi</a>
+    //           </Navbar.Text>
+    //         </li>
+    //         <Navbar.Text style={{ margin: "10px" }}>
+    //           <a href="">Alpha Epsilon Pi</a>
+    //         </Navbar.Text>
+    //         <Navbar.Text style={{ margin: "10px" }}>
+    //           <a href="">Meet the Central Comm.</a>
+    //         </Navbar.Text>
+    //         <Navbar.Text style={{ margin: "10px" }}>
+    //           <a href="">Our manifesto</a>
+    //         </Navbar.Text>
+    //         <Navbar.Text style={{ margin: "10px" }}>
+    //           <a href="">Join the CCP!</a>
+    //         </Navbar.Text>
+    //       </ul>
+    //     </Navbar.Collapse>
+    //   </Navbar>
+    // </div>
+    <div class="wrapper">
+      <nav id="sidebar">
+        <div class="sidebar-header">
+          <Image src={Bofa} alt="..." width="220px" height="273px" />
+        </div>
+
+        <ul class="list-unstyled components">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#">Executive Board</a>
+          </li>
+          <li>
+            <a href="#">Philanthropy</a>
+          </li>
+          <li>
+            <a href="#">Rush</a>
+          </li>
+          <li>
+            <a href="#">Contact Information</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
--- src/pages/InitPage.js
+++ src/pages/InitPage.js
@@ -2,12 +2,17 @@ import React from "react";
 import PageTitle from "../components/PageTitle";
 import Init from "../components/Init";
 import MainNavBar from "../components/MainNavBar";
+import Container from "react-bootstrap/esm/Container";
 const InitPage = () => {
   return (
-    <div>
-      <MainNavBar />
-      <PageTitle />
-      <Init />
+    <div className=" pageBackground fillBackground">
+      {/* the div wrapper will place the side bar with other elements  */}
+      <div className="wrapper">
+        <MainNavBar />
+
+        <PageTitle />
+        <Init />
+      </div>
     </div>
   );
 };
