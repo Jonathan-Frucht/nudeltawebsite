bb0057c2940ba9619da5146fff6b1e5d53707668
Jonathan Frucht <jonfrucht@gmail.com>
2020-12-17 15:49:13 -0500

    navbar now routes to pages

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -7,36 +7,6 @@ import Bofa from "../pictures/COFA_Color.png";
 import Image from "react-bootstrap/Image";
 function MainNavBar() {
   return (
-    // <div className="wrapper">
-    //   <Navbar id="sidebar">
-    //     <Navbar.Brand>
-    //       <img src={Bofa} alt="..." width="220px" height="273px" />
-    //     </Navbar.Brand>
-
-    //     <Navbar.Collapse id="content">
-    //       <ul>
-    //         <li>
-    //           {" "}
-    //           <Navbar.Text style={{ margin: "10px" }}>
-    //             <a href="">Alpha Epsilon Pi</a>
-    //           </Navbar.Text>
-    //         </li>
-    //         <Navbar.Text style={{ margin: "10px" }}>
-    //           <a href="">Alpha Epsilon Pi</a>
-    //         </Navbar.Text>
-    //         <Navbar.Text style={{ margin: "10px" }}>
-    //           <a href="">Meet the Central Comm.</a>
-    //         </Navbar.Text>
-    //         <Navbar.Text style={{ margin: "10px" }}>
-    //           <a href="">Our manifesto</a>
-    //         </Navbar.Text>
-    //         <Navbar.Text style={{ margin: "10px" }}>
-    //           <a href="">Join the CCP!</a>
-    //         </Navbar.Text>
-    //       </ul>
-    //     </Navbar.Collapse>
-    //   </Navbar>
-    // </div>
     <div class="wrapper">
       <nav id="sidebar">
         <div class="sidebar-header">
@@ -48,16 +18,16 @@ function MainNavBar() {
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="#">Executive Board</a>
+            <a href="/exec_board">Executive Board</a>
           </li>
           <li>
-            <a href="#">Philanthropy</a>
+            <a href="/philanthropy">Philanthropy</a>
           </li>
           <li>
-            <a href="#">Rush</a>
+            <a href="/rush">Rush</a>
           </li>
           <li>
-            <a href="#">Contact Information</a>
+            <a href="/contact_info">Contact Information</a>
           </li>
         </ul>
       </nav>
