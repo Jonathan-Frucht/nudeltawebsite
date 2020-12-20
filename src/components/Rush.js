3e637fe623bd8c8b8a23c408907ead55640be834
Jon <Jonfrucht@gmail.com>
2020-12-20 13:57:33 -0500

    Added rush page + layout

---
+++ src/components/Rush.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+function Rush() {
+  return ( 
+      <Container >
+<div className="rushWrapper" >
+          <div>
+          <iframe 
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfVDBCn9yNZE6N-MeGw9wkyP3IXEbunSRCPpzThF3KisDCGdA/viewform?embedded=true" 
+          width="640" 
+          height="1040" 
+          frameborder="0" 
+          marginheight="0"
+           marginwidth="0">Loading…</iframe>
+          </div>
+      </div>
+      </Container>
+      
+  );
+}
+
+export default Rush;
---
+++ src/pages/RushPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Rush from "../components/Rush";
+import MainNavBar from "../components/MainNavBar";
+import Container from "react-bootstrap/esm/Container";
+import { Col } from "react-bootstrap";
+const RushPage = () => {
+  return (
+    <div className=" ">
+      {/* the div wrapper will place the side bar with other elements  */}
+      <div className="">
+        <Container fluid>
+          <Col xs="1">
+          <div >
+          <MainNavBar />
+        </div>
+          </Col>
+          
+          <Col fluid>
+          <div style={{backgroundColor:"grey"}} >
+          <Rush />
+        </div>
+          </Col>
+        </Container>
+     
+       
+      </div>
+    </div>
+  );
+};
+
+export default RushPage;
