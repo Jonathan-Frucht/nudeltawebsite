0ad861ee246186de978834008acd074b056dc626
Jon <Jonfrucht@gmail.com>
2020-12-26 23:23:25 -0500

    Saving outdated NavBar

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -5,7 +5,10 @@ import Image from "react-bootstrap/Image";
 function MainNavBar() {
   return (
     <div class="wrapper">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>   
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
 
       <nav id="sidebar">
         <div class="sidebar-header">
@@ -16,21 +19,41 @@ function MainNavBar() {
           <li>
             <a href="/">Home</a>
           </li>
-          <li>
-            <a href="/exec_board">Executive Board</a>
+          <li id="visibleA">
+            <a href="/alumni">Alumni</a>
+            <ul id="hiddenA">
+              <li>
+                <a href="/rush_info">Newsletter</a>
+              </li>
+              <li>
+                <a href="/rush_form">Update Info</a>
+              </li>
+            </ul>
           </li>
           <li>
             <a href="/philanthropy">Philanthropy</a>
           </li>
-          <li>
-            <a href="/rush">Rush</a>
+          <li id="visibleR">
+            <a href="/rush_info">Rush</a>
+            <ul id="hiddenR">
+              <li>
+                <a href="/rush_info">What is Rush?</a>
+              </li>
+              <li>
+                <a href="/rush_form">Sign up for Rush</a>
+              </li>
+            </ul>
           </li>
           <li>
             <a href="/contact_info">Contact Information</a>
           </li>
           <li id="instagram" style={{}}>
-          <a href="https://www.instagram.com/aepicf/" target="_blank" rel="noreferrer" class="fa fa-instagram"></a>  
-
+            <a
+              href="https://www.instagram.com/aepicf/"
+              target="_blank"
+              rel="noreferrer"
+              class="fa fa-instagram"
+            ></a>
           </li>
         </ul>
       </nav>
